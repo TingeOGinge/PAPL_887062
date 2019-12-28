640cd0df34fb158758838fb6cd2311adea408584
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2019-12-28 14:03:36 +0000

    node01 page with functionality to return to Main

--- app/src/main/java/com/example/papl_887062/Node01.java
+++ app/src/main/java/com/example/papl_887062/Node01.java
@@ -2,13 +2,20 @@ package com.example.papl_887062;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Node01 extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_first_node);
+    setContentView(R.layout.activity_node01);
+  }
+
+  public void onClickBack(View view) {
+    Intent node01ToMain = new Intent(Node01.this, MainActivity.class);
+    startActivity(node01ToMain);
   }
 }
