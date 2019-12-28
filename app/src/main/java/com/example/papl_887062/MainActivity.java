cb3f32250628edfb51a4cfe42fe6b00ea3305e28
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2019-12-28 13:17:46 +0000

    First commit

---
+++ app/src/main/java/com/example/papl_887062/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.papl_887062;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.*;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  public void onClickExitButton(View view) {
+    System.exit(0);
+  }
+
+  public void onClickHomepageText(View view) {
+    Intent myIntent = new Intent(MainActivity.this, Node01.class);
+    startActivity(myIntent);
+  }
+}
---
+++ app/src/main/java/com/example/papl_887062/Node01.java
@@ -0,0 +1,14 @@
+package com.example.papl_887062;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Node01 extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_first_node);
+  }
+}
