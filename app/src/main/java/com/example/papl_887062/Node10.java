12fe22c69d241fb42422edd05684405964bf4244
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2019-12-29 20:20:14 +0000

    add node10 including option 1

---
+++ app/src/main/java/com/example/papl_887062/Node10.java
@@ -0,0 +1,54 @@
+package com.example.papl_887062;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Node10 extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_node10);
+    ((DecisionStack) this.getApplication()).add(Node10.class);
+  }
+
+  public void onClickBack(View view) {
+    Class previous = ((DecisionStack) this.getApplication()).getPrevious(true);
+    try {
+      Intent node01ToMain = new Intent(Node10.this, previous);
+      startActivity(node01ToMain);
+    } catch (Exception e) {
+      System.err.println(e);
+    }
+  }
+
+  public void onClickOption1(View view) {
+    openOptionDialog(view, Message09.class);
+  }
+
+  public void openOptionDialog(View view, final Class destination) {
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setTitle("Option Description")
+        .setMessage(view.getContentDescription())
+        .setPositiveButton("Select", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialogInterface, int i) {
+            dialogInterface.cancel();
+            Intent in = new Intent(Node10.this, destination);
+            startActivity(in);
+          }
+        })
+        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialogInterface, int i) {
+            dialogInterface.cancel();
+          }
+        });
+    builder.show();
+  }
+}
