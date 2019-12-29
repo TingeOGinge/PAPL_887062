ab9a8f21a7f67ca0fda48711395c1866a7861358
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2019-12-29 17:52:26 +0000

    add options 4&5 to Node02

--- app/src/main/java/com/example/papl_887062/Node05.java
+++ app/src/main/java/com/example/papl_887062/Node05.java
@@ -6,24 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Message04 extends AppCompatActivity {
+public class Node05 extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_message04);
-  }
-
-  public void onClickMessage(View view) {
-    Intent myIntent = new Intent(Message04.this, Message07.class);
-    startActivity(myIntent);
+    setContentView(R.layout.activity_node05);
+    ((DecisionStack) this.getApplication()).add(Node05.class);
   }
 
   public void onClickBack(View view) {
     Class previous = ((DecisionStack) this.getApplication()).getPrevious();
     try {
-      Intent i = new Intent(Message04.this, previous);
-      startActivity(i);
+      Intent node01ToMain = new Intent(Node05.this, previous);
+      startActivity(node01ToMain);
     } catch (Exception e) {
       System.err.println(e);
     }
