4bf38f1ec1311aa06789c309d53da78a57f38bf6
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2019-12-29 18:17:33 +0000

    add options 6&7 to Node02

--- app/src/main/java/com/example/papl_887062/Message09.java
+++ app/src/main/java/com/example/papl_887062/Message09.java
@@ -6,23 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Message05 extends AppCompatActivity {
+public class Message09 extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_message05);
+    setContentView(R.layout.activity_message09);
   }
 
   public void onClickMessage(View view) {
-    Intent myIntent = new Intent(Message05.this, Node06.class);
+    Intent myIntent = new Intent(Message09.this, MainActivity.class);
     startActivity(myIntent);
   }
 
   public void onClickBack(View view) {
     Class previous = ((DecisionStack) this.getApplication()).getPrevious();
     try {
-      Intent i = new Intent(Message05.this, previous);
+      Intent i = new Intent(Message09.this, previous);
       startActivity(i);
     } catch (Exception e) {
       System.err.println(e);
