4ad106902a27d9d0a64dd3da2affe482108bdc95
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2019-12-30 13:33:59 +0000

    :art: restructure tailored messages

--- app/src/main/java/com/example/papl_887062/Node04.java
+++ app/src/main/java/com/example/papl_887062/Node04.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.view.View;
 
 public class Node04 extends AppCompatActivity {
@@ -18,10 +19,15 @@ public class Node04 extends AppCompatActivity {
   }
 
   public void onClickOption1(View view) {
+    InputCalculation.setMessage(getResources().getString(R.string.node04_01_TMessage));
     openOptionDialog(view, ExerciseThen.class);
   }
 
   public void onClickOption2(View view) {
+    InputCalculation.setMessage(
+        "Being unwell sucks. You've made the right decision not to exercise today.\n" +
+            "Drink plenty of fluids and get plenty of rest"
+    );
     openOptionDialog(view, Message06.class);
   }
 
